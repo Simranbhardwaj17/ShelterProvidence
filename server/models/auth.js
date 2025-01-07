f21da82bb84693c48e6963fcecdbc5108acd5a92
Simranbhardwaj17 <simranbhardwaj1706@gmail.com>
2025-01-07 22:15:24 +0530

    User model DONE!!

--- server/models/auth.js
+++ server/models/auth.js
@@ -1,4 +1,4 @@
-import {model, Schema, ObjecId} from "mongoose";      //destructure model, schema
+import {model, Schema, ObjectId} from "mongoose";      //destructure model, schema
 
 //create new instance of schema
 const schema = new Schema({
@@ -36,13 +36,18 @@ const schema = new Schema({
         default: ["Buyer"],
         enum: ["Buyer", "Seller", "Admin"],   //one of these
     },
-    enquiredProperties: [{    //when user login to dashboard we can show previously inquired prop
-
-    }]   
-})
+    enquiredProperties: [{ type: ObjectId, ref: "Ad" }],   //when user login to dashboard we can show previously inquired prop
+                                             //just save the prop id as each prop hv differ one & refer to ad model(it can be house, property, land, for rent/sale)
+    wishlist: [{ type: ObjectId, ref: "Ad" }],    //save ObjId & refer to ad
+    resetCode: "",  //after req generate token & send on mail then save that in DB
+ },
+ { timestamps: true }   //automatic update time in DB(each time when update occur)
+);
 
 export default model("User", schema)   //user model based on schema u wi8
 //think it as table in DB
 //export it as user model, interact with user ,update, dlt
 
 //in that way we can create schema & export as model ,so that we can use it to interact with DB
+
+//User model DONE!!
