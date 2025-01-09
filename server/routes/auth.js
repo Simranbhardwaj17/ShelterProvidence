ac92f3864a18754b10ab1062ab5abf8b5e224e38
Simranbhardwaj17 <simranbhardwaj1706@gmail.com>
2025-01-09 23:13:05 +0530

    User registration route created

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -12,6 +12,7 @@ import * as auth from "../controllers/auth.js";
 //delete:- to dlt
 //create router & see how we can send response
 // router.get('/', welcome );    //1st arg of get is url- listen to API, 2nd arg is callbk fn(have access to request & res obj) so express can be called as req & res handler
-router.get('/', auth.welcome );
+router.get("/", auth.welcome );
+router.post("/pre-register", auth.preRegister)      //create route for user reg(pre-reg coz don't immediately save user into DB, 1st send cnf email then, else anyone can use garbage email)
 
 export default router;        //to import in our server
