8b72189f3398a58924d20b4298e08558317e08f8
Simranbhardwaj17 <simranbhardwaj1706@gmail.com>
2025-01-11 23:11:20 +0530

    To check if user provided valid email address

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -7,10 +7,20 @@ export const welcome = (req, res) => {
 };
 
 export const preRegister = async (req, res) => {
+    //create jwt with email and password then email as clickable link
+    //only ehwn user click on that email link, reg complete
     try{
-        //catch
+        console.log(req.body);
+
+        const emailSent = true;
+
+        if(emailSent) {
+            return res.json({ok: true });
+        } else{
+            return res.json({ok: false});
+        }
     } catch (err) {
         console.log(err);
-        return res.json({error: "Something went wrong. Try Again!"})
+        return res.json({error: "Something went wrong. Try Again!"});
     }
 }
